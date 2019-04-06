f4998464183b3304c8c26cd6397341dbf2771915
Jeston Teo <jestontzh@gmail.com>
2019-04-07 02:12:46 +0800

    Added listener for SearchView

--- app/src/main/java/com/example/lomoimagegallery/MainActivity.java
+++ app/src/main/java/com/example/lomoimagegallery/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.lomoimagegallery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private String searchQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,27 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_activity_menu, menu);
+
+        MenuItem searchMenuItem = menu.findItem(R.id.search_button);
+        SearchView searchView = (SearchView) searchMenuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.i(TAG, String.format("String captured: %s", query));
+                searchQuery = query;
+
+                // TODO: Perform call with query to Pixabay
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
         return true;
     }
+
+
 }
