4fda47ea12c7880eb046c31cb6b7da4295f30e74
Jeston Teo <jestontzh@gmail.com>
2019-04-07 01:39:30 +0800

    Added 'Search' in App Bar

--- app/src/main/java/com/example/lomoimagegallery/MainActivity.java
+++ app/src/main/java/com/example/lomoimagegallery/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.lomoimagegallery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu, menu);
+        return true;
     }
 }
