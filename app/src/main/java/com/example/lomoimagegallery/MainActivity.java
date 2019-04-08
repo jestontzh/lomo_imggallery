c034feef4623d08d9435d92167f5eb66f39fb2a6
Jeston Teo <jestontzh@gmail.com>
2019-04-09 02:25:54 +0800

    Refactored REST call to Pixabay API

--- app/src/main/java/com/example/lomoimagegallery/MainActivity.java
+++ app/src/main/java/com/example/lomoimagegallery/MainActivity.java
@@ -22,8 +22,10 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -34,11 +36,17 @@ public class MainActivity extends AppCompatActivity {
     private static final String API_KEY = "12114072-7845e9d84252d3611d51cff25";
     private static final int RESULTS_PER_PAGE = 30;
 
+    private static final int SWIPE_THRESHOLD = 100;
+    private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
     private ProgressBar progressBar;
     private StaggeredGridLayoutManager sglManager;
     private RecyclerView recyclerView;
     private int spanCount;
     private int pageNum = 1; // default to 1
+
+    private float downX;
+    private float upX;
 
     private String searchQuery;
     private List<PixalbayImages> mList;
@@ -93,25 +101,7 @@ public class MainActivity extends AppCompatActivity {
 
                 progressBar.setVisibility(View.VISIBLE);
 
-                Call<HttpResponse> call = HttpRequestClient.getClient().create(HttpRequest.class).requestImages(API_KEY, convertQuery(searchQuery), pageNum, RESULTS_PER_PAGE);
-                call.enqueue(new Callback<HttpResponse>() {
-                    @Override
-                    public void onResponse(Call<HttpResponse> call, Response<HttpResponse> response) {
-                        Log.i(TAG, String.format("%b %d", response.isSuccessful(), response.code()));
-                        mList = response.body().getHits();
-
-                        progressBar.setVisibility(View.INVISIBLE);
-
-                        PixabayImageRecyclerViewAdapter rcAdapter = new PixabayImageRecyclerViewAdapter(MainActivity.this, mList);
-                        recyclerView.setAdapter(rcAdapter);
-                    }
-
-                    @Override
-                    public void onFailure(Call<HttpResponse> call, Throwable t) {
-                        Log.i(TAG, "Something went wrong. What?!");
-                        Log.i(TAG, t.getMessage());
-                    }
-                });
+                loadImages();
 
                 return true;
             }
@@ -122,6 +112,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         return true;
+    }
+
+    private void loadImages() {
+        Call<HttpResponse> call = HttpRequestClient.getClient().create(HttpRequest.class).requestImages(API_KEY, convertQuery(searchQuery), pageNum, RESULTS_PER_PAGE);
+        call.enqueue(new Callback<HttpResponse>() {
+            @Override
+            public void onResponse(Call<HttpResponse> call, Response<HttpResponse> response) {
+                Log.i(TAG, String.format("%b %d", response.isSuccessful(), response.code()));
+                progressBar.setVisibility(View.INVISIBLE);
+                try {
+                    mList = response.body().getHits();
+                    PixabayImageRecyclerViewAdapter rcAdapter = new PixabayImageRecyclerViewAdapter(MainActivity.this, mList);
+                    recyclerView.setAdapter(rcAdapter);
+                } catch (NullPointerException e) {
+                    if (pageNum == 1) {
+                        Toast.makeText(getApplicationContext(), "No result for your query.", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "No results left for your query", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            @Override
+            public void onFailure(Call<HttpResponse> call, Throwable t) {
+                Log.i(TAG, "Query Failure - Try again later");
+                Log.i(TAG, t.getMessage());
+                Toast.makeText(getApplicationContext(), "Something went wrong. Please try again later.", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
